f40373fdc8fdc7ed681412d14ef721a528ec6de4
angelespini <angelesbpini@gmail.com>
2022-06-03 11:21:12 -0300

    js

--- js/show.js
+++ js/show.js
@@ -1,21 +1,31 @@
-function muestra_oculta(id){
-    if (document.getElementById){ 
-    var el = document.getElementById(id); 
-    el.style.display = (el.style.display == 'none') ? 'block' : 'none'; 
+function muestra_oculta(id) {
+    if (document.getElementById) {
+        var el = document.getElementById(id);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
     }
-    }
-    window.onload = function(){
+}
+window.onload = function() {
     muestra_oculta('contenido');
+}
+
+let boton = document.getElementById('ver-menos');
+
+let contador = 0
+
+function ver_menos() {
+    contador++
+    if (contador > 1) {
+        contador = 0
     }
 
-    let boton = document.getElementById('ver-menos');
-
-    
-    function ver_menos(){
-        boton.innerHTML='Ver menos'
+    if (contador === 0) {
+        boton.innerHTML = 'Ver más'
+    } else {
+        boton.innerHTML = 'Ver menos'
     }
+}
 
-    function cartel(contenido){
-        ver_menos();
-         muestra_oculta(contenido);
-    }
+function cartel(contenido) {
+    ver_menos();
+    muestra_oculta(contenido);
+}
